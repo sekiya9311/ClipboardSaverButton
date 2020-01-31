08bf96916c998287b511b1a87825d2bb6db542c3
sekiya9311 <sekiya9311@gmail.com>
2020-02-01 00:42:51 +0900

    Add project files.

---
+++ ClipboardSaverButton/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using ClipboardSaverButton.Views;
+using System.Windows;
+
+namespace ClipboardSaverButton
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
