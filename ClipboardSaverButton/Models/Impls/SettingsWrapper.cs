cda7dc0d9e7088d38000309265addeec3e083390
sekiya9311 <sekiya9311@gmail.com>
2020-02-06 21:53:07 +0900

    Add model class of setting

---
+++ ClipboardSaverButton/Models/Impls/SettingsWrapper.cs
@@ -0,0 +1,25 @@
+﻿using ClipboardSaverButton.Models.Interfaces;
+using ClipboardSaverButton.Properties;
+
+namespace ClipboardSaverButton.Models.Impls
+{
+    public class SettingsWrapper : ISettings
+    {
+        public double MainWindowLeft { get; set; }
+        public double MainWindowTop { get; set; }
+
+        public SettingsWrapper()
+        {
+            MainWindowLeft = Settings.Default.MainWindowLeft;
+            MainWindowTop = Settings.Default.MainWindowTop;
+        }
+
+        public void Save()
+        {
+            Settings.Default.MainWindowLeft = MainWindowLeft;
+            Settings.Default.MainWindowTop = MainWindowTop;
+
+            Settings.Default.Save();
+        }
+    }
+}
--- ClipboardSaverButton/Views/MainWindow.xaml.cs
+++ ClipboardSaverButton/Views/MainWindow.xaml.cs
@@ -11,20 +11,6 @@ namespace ClipboardSaverButton.Views
         public MainWindow()
         {
             InitializeComponent();
-
-            if (Properties.Settings.Default.MainWindowLeft != 0)
-            {
-                this.Left = Properties.Settings.Default.MainWindowLeft;
-                this.Top = Properties.Settings.Default.MainWindowTop;
-            }
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            Properties.Settings.Default.MainWindowLeft = this.Left;
-            Properties.Settings.Default.MainWindowTop = this.Top;
-
-            Properties.Settings.Default.Save();
         }
     }
 }
